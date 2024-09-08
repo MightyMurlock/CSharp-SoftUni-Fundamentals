bf68fb90c2de7b1a2cfa82a67e6487c3915f7c6a
Konstantin <63278826+MightyMurlock@users.noreply.github.com>
2024-09-08 18:40:04 +0300

    more changes

---
+++ Basic Syntax Exercise/P02/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int option = 0;
+
+            if (number % 2 == 0)
+            {
+                option = 2;
+            }
+            if (number % 3 == 0)
+            {
+                option = 3;
+            }
+            if (number % 6 == 0)
+            {
+                option = 6;
+            }
+            if (number % 7 == 0)
+            {
+                option = 7;
+            }
+            if (number % 10 == 0)
+            {
+                option = 10;
+            }
+
+            switch (option)
+            {
+                case 2:
+                    Console.WriteLine($"The number is divisible by 2");
+                    break;
+                case 3:
+                    Console.WriteLine($"The number is divisible by 3");
+                    break;
+                case 6:
+                    Console.WriteLine($"The number is divisible by 6");
+                    break;
+                case 7:
+                    Console.WriteLine($"The number is divisible by 7");
+                    break;
+                case 10:
+                    Console.WriteLine($"The number is divisible by 10");
+                    break;
+                default:
+                    Console.WriteLine("Not divisible");
+                    break;
+            }
+        }
+    }
+}
---
+++ Basic Syntax Exercise/P03/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine(); 
+            string day = Console.ReadLine();
+
+            double price = 0;
+            switch (day)
+            {
+                case "Friday":
+                    if (type == "Students")
+                    {
+                        price = 8.45;
+                    }
+                    else if (type == "Business")
+                    {
+                        price = 10.90;
+                    }
+                    else if (type == "Regular")
+                    {
+                        price = 15;
+                    }
+                    break;
+                case "Saturday":
+                    if (type == "Students")
+                    {
+                        price = 9.80;
+                    }
+                    else if (type == "Business")
+                    {
+                        price = 15.60;
+                    }
+                    else if (type == "Regular")
+                    {
+                        price = 20;
+                    }
+                    break;
+                case "Sunday":
+                    if (type == "Students")
+                    {
+                        price = 10.46;
+                    }
+                    else if (type == "Business")
+                    {
+                        price = 16;
+                    }
+                    else if (type == "Regular")
+                    {
+                        price = 22.50;
+                    }
+                    break;
+            }
+
+            if (type == "Students" && peopleCount >= 30)
+            {
+                price -= (price * 0.15);
+            }
+            if (type == "Business" && peopleCount >= 100)
+            {
+                peopleCount -= 10;
+            }
+            if (type == "Regular" && peopleCount >= 10 && peopleCount <= 20)
+            {
+                price -= (price * 0.05);
+            }
+
+            price *= peopleCount;
+
+            Console.WriteLine($"Total price: {price:f2}");
+
+        }
+    }
+}
